2abcd24497b6aaee83e29e61c395af6ea1f9be6b
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2019-12-02 17:59:31 -0600

    making process on form validation js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,63 @@
+// document.getElementById("my-success-alert").classList.remove("d-none");
+// document.getElementById("my-success-alert").classList.remove("d-none");
+// document.getElementsByClassName("alert-success")[0].classList.remove("d-none");
+
 function myAppOnLoad() {
     $('alert').alert();
 }
 
+
+
+
 document.addEventListener('onload', myAppOnLoad);
 
-document.getElementById("contact").addEventListener("submit", (e) => {
+document.getElementById("contact-form").addEventListener("submit", (e) => {
     e.preventDefault(); //
+    if (test_valid_form()) {
+        document.getElementById("my-success-alert").classList.remove("d-none");
+    }
 });
+
+
+
+
+// Using https://developer.mozilla.org/en-US/docs/Learn/HTML/Forms/Form_validation for guidance on this part
+let form = document.getElementById('contact-form');
+let email = document.getElementById("contact-email");
+let success = document.getElementById("my-success-alert");
+let error = document.getElementById( "my-failure-alert");
+
+
+
+function test_valid_form() {
+    let form = document.getElementById('contact-form');
+    // does the input with id = "contact-music" make sense
+    // I'm just gonna say it should not be empty
+    if (document.getElementById("contact-music").value == "") {
+        return false
+    } else if (document.getElementById(""))
+        console.log(document.getElementById("contact-music").value);
+        console.log("oops");
+    }
+    return true;
+}
+
+
+email.addEventListener("input", function() {
+    if (test_valid_form()) {
+        document.getElementById("my-success-alert").classList.remove("d-none");
+        // success.classList.remove("d-none");
+        // error.classList.add("d-none)");
+    }
+
+});
+
+function revealSuccessAlert() {
+    document.getElementById("my-success-alert").classList.remove("d-none");
+}
+
+
+// document.getElementById("my-success-alert").classList.remove("d-none");
+// document.getElementById("my-success-alert").classList.add("d-none)");
+// document.getElementById("my-success-alert").classList.remove("d-none");
+// document.getElementById("my-success-alert).classList.add("d-none)");
